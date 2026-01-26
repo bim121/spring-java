e8cefa152a8b11fb776295b14bd12e525c4e2194
Vitaliy Tishik <vitisik54@gmail.com>
2026-01-27 00:38:19 +0200

    ref

--- src/main/java/org/example/repositories/impl/BookRepositoryImpl.java
+++ src/main/java/org/example/repositories/impl/BookRepositoryImpl.java
@@ -1,23 +1,31 @@
 package org.example.repositories.impl;
 
-import java.util.ArrayList;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.example.model.Book;
 import org.example.repositories.BookRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
+@Transactional
 public class BookRepositoryImpl implements BookRepository {
-    private List<Book> books = new ArrayList<>();
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public Book save(Book book) {
-        books.add(book);
+        entityManager.persist(book);
         return book;
     }
 
     @Override
     public List<Book> findAll() {
-        return books;
+        return entityManager
+                .createQuery("from Book", Book.class)
+                .getResultList();
     }
 }
